213828daadaf493cebdfd04c5cf0c018ae441485
Jon Pretty <jon.pretty@propensive.com>
2024-07-01 08:13:09 +0200

    Renamed `msg""` to `m""`

--- src/core/modicum.scala
+++ src/core/modicum.scala
@@ -26,20 +26,20 @@ import scala.collection.mutable as scm
 import language.experimental.captureChecking
 
 case class MemoryError(amount: ByteSize)
-extends Error(msg"a request was made for $amount more memory than has been allocated")
+extends Error(m"a request was made for $amount more memory than has been allocated")
 
 case class Allowance(amount: ByteSize, retain: ByteSize):
   var free: ByteSize = amount
   var retained: ByteSize = 0.b
   val blocks: scm.Map[ByteSize, List[Array[Byte]]] = scm.HashMap().withDefault { _ => Nil }
-  
+
   def allocate(requirement: ByteSize): Array[Byte] raises MemoryError = synchronized:
     if requirement < free then blocks(requirement) match
       case head :: tail =>
         blocks(requirement) = tail
         free -= requirement
         head
-      
+
       case Nil =>
         free -= requirement
         new Array[Byte](requirement.long.toInt)
@@ -49,7 +49,7 @@ case class Allowance(amount: ByteSize, retain: ByteSize):
     synchronized:
       val size = array.size.b
       free += size
-      
+
       if retained < retain then
         blocks(size) ::= array
         retained += size
@@ -64,7 +64,6 @@ def allocate
     (using allowance: Allowance, excessMemory: Errant[MemoryError])
     (block: Array[Byte]^ => ResultType)
     : ResultType =
-  
+
   val array = allowance.allocate(amount)
   block(array).also(allowance.deallocate(array))
-  
